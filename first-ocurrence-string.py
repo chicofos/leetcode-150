e578dee688a69baee2e4046732059ab7a8a1742f
Francisco Ochoa <fco.ochoas@gmail.com>
2025-05-07 11:57:59 -0700

    first ocurrence string

---
+++ first-ocurrence-string.py
@@ -0,0 +1,11 @@
+def strStr(haystack, needle):
+     #--------------------------------------------------
+    # Time Complexity: O(m+n)
+    # Space Complexity: O(1)
+    #--------------------------------------------------
+    return haystack.find(needle)
+    
+
+
+print(strStr('sadbutsad','sad'))
+print(strStr('leetcode','leeto'))
